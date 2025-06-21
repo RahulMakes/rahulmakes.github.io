3990b374d562b1ccfaaba88674ffb163b0dcdd47
RahulMakes <rahul31kumar31@gmail.com>
2025-06-21 19:43:49 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+
+document.querySelectorAll(".link")[0].addEventListener("click",()=>{
+    window.location.href="TossCoin"
+})
+
+document.querySelectorAll(".link")[1].addEventListener("click",()=>{
+    window.location.href="AnswerIndia"
+})
+let i = 0;
+setInterval(() => {
+    document.body.style.background = `linear-gradient(${i}deg,rgb(255, 81, 0) 1%,rgb(125, 240, 163) 100%)`;
+    i += 1;
+}, 30);
