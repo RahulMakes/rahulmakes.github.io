5c875641e94a53cd245ba9d57deb6bea2c85893b
RahulMakes <rahul31kumar31@gmail.com>
2025-06-26 23:46:45 +0530

    Update script.js

--- script.js
+++ script.js
@@ -6,6 +6,9 @@ document.querySelectorAll(".link")[0].addEventListener("click",()=>{
 document.querySelectorAll(".link")[1].addEventListener("click",()=>{
     window.location.href="AnswerIndia"
 })
+document.querySelectorAll(".link")[2].addEventListener("click",()=>{
+    window.location.href="random"
+})
 let i = 0;
 setInterval(() => {
     document.body.style.background = `linear-gradient(${i}deg,rgb(255, 81, 0) 1%,rgb(125, 240, 163) 100%)`;
